07e72f124890a4a46c6cf557f974e5f93cfffcfa
AIDARXAN <mainwinnertactics@gmail.com>
2019-10-20 23:49:08 +0600

    ADD custom markers

--- app/src/main/java/com/map/MapsActivity.java
+++ app/src/main/java/com/map/MapsActivity.java
@@ -1,19 +1,38 @@
 package com.map;
 
+import androidx.annotation.DrawableRes;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements GoogleMap.OnMarkerClickListener, OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    private BitmapDescriptor bitmapDescriptorFromVector(Context context, @DrawableRes int vectorResId) {
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
+        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +60,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .draggable(true)
+                .title("Sydney"))
+                .setIcon(bitmapDescriptorFromVector(this, R.drawable.ic_australia));
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(78.765481, 18.422899))
+                .draggable(true)
+                .title("Svalbard"))
+                .setIcon(bitmapDescriptorFromVector(this, R.drawable.ic_norway));
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(54.974798, 83.135570))
+                .draggable(true)
+                .title("Novosibirsk"))
+                .setIcon(bitmapDescriptorFromVector(this, R.drawable.ic_russia));
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(54.733295, 56.036891))
+                .draggable(true)
+                .title("Ufa"))
+                .setIcon(bitmapDescriptorFromVector(this, R.drawable.ic_russia));
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(42.842477, 74.589037))
+                .draggable(true)
+                .title("Bishkek"))
+                .setIcon(bitmapDescriptorFromVector(this, R.drawable.ic_kyrgyzstan));
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(64.143269, -17.846021))
+                .draggable(true)
+                .title("Iceland"))
+                .setIcon(bitmapDescriptorFromVector(this, R.drawable.ic_iceland));
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(-54.857476, -64.588401))
+                .draggable(true)
+                .title("Isla de los Estados"))
+                .setIcon(bitmapDescriptorFromVector(this, R.drawable.ic_argentina));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        return false;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
